9b7d10fe32e2f7fd025f2b8f9cadc3ba26cf8030
Zhoro Slavchev <zgslavchev@gmail.com>
2021-09-02 11:53:54 +0300

    change

--- DiceGame/assets/js/index.js
+++ DiceGame/assets/js/index.js
@@ -1,18 +1,17 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+const randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+const randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "assets/img/" + randomDiceImage; //images/dice1.png - images/dice6.png
+const randomImageSource = "assets/img/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+const image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "assets/img/dice" + randomNumber2 + ".png";
+const randomImageSource2 = "assets/img/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
