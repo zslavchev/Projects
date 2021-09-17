328dcd8ad9453f4a8714ef32edc9bf91a7ee0b68
Zhoro Slavchev <zgslavchev@gmail.com>
2021-09-17 19:23:32 +0300

    refactor dices

--- Dice Game/assets/js/index.js
+++ Dice Game/assets/js/index.js
@@ -1,28 +1,28 @@
-
-const randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-const randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-const randomImageSource = "assets/img/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-const image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
+// Const random numbers 1 to 6;
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+//Randomize dice images;
+const randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+const randomImageSource = "assets/img/" + randomDiceImage; //images/dice1.png - images/dice6.png
 const randomImageSource2 = "assets/img/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+//Set images for the dices;
+const image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+const image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
+//If player 1 wins;
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
+//If player 2 wins;
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
+//If it is draw;
 else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
