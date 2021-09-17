9f195c103dec8d9781a849879f3635caef0773ff
Zhoro Slavchev <zgslavchev@gmail.com>
2021-09-17 19:54:31 +0300

    refactor tindog

--- TinDog/assets/js/footerDynamicDate.js
+++ TinDog/assets/js/footerDynamicDate.js
@@ -1,7 +1,9 @@
+//Add dynamic date to footer function;
 const date = function () {
     let time = new Date().getFullYear();
     return time;
 }
 
+//Display the dinamyc date;
 document.querySelector(".footer-year").innerHTML = `© Copyright ${date()} TinDog`;
 
