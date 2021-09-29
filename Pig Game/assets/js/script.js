642bbfac947a0d26e9fb99f1e2728cfc63262348
Zhoro Slavchev <zgslavchev@gmail.com>
2021-09-29 20:46:50 +0300

    changes regarding netlify

--- Pig Game/assets/js/script.js
+++ Pig Game/assets/js/script.js
@@ -53,7 +53,7 @@ btnRoll.addEventListener('click', function () {
 
         //2. Display dice;
         diceEl.classList.remove('hidden');
-        diceEl.src = `dice-${dice}.png`;
+        diceEl.src = `assets/img/dice-${dice}.png`
 
         //3. Check for rolled 1;
         if (dice !== 1) {
